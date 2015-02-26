03269eabd9dd800d56aab3d0986ec7eb2a27b2c7
zfc5840 <1076231986@qq.com>
2015-02-26 16:51:57 +0800

    创建commons-lang3包项目，用于测试此jar包提供的一系列公用方法

---
+++ src/main/java/com/commons_lang/commons_langDemo/Demo.java
@@ -0,0 +1,15 @@
+/**
+ * Description: Demo.java
+ * All Rights Reserved.
+ * @version 1.0  2015-2-26 下午4:26:49  by 张富成（fc.zhang@zuche.com）创建
+ */
+package com.commons_lang.commons_langDemo;
+
+/**
+ * Description: 
+ * All Rights Reserved.
+ * @version 1.0  2015-2-26 下午4:26:49  by 张富成（fc.zhang@zuche.com）创建
+ */
+public class Demo {
+
+}
